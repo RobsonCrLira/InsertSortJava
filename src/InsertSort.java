9c184b7c9ff492536abeaea09ac80ae971bbfaff
Robso <robso.crlira@gmail.com>
2020-03-09 08:19:04 -0300

    Termino da atividade e adicionando gerador de numeros aleatorios e

--- src/InsertSort.java
+++ src/InsertSort.java
@@ -1,28 +1,68 @@
+import java.util.Scanner;
+
 public class InsertSort {
 
     public static void main(String[] args){
 
-        int lista[] = new int[n];
-        lista = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int lista[] = new int[10];
+        int op;
 
+        for(int i = 0; i<lista.length;i++)
+            lista[i] = (int) (Math.random()*999);
+        imprimir(lista);
 
-    }
-    public void insertSortAsc() {
-        int n=10 ,j;
-        for (j=1 ; j<n ; j++){
-            int aux = lista[j];
-            int i = j-1;
+        Scanner ent = new Scanner(System.in);
+        System.out.println("Digite 1 Ordem Crescente e imprimir  \nDigite 2 Ordem Descresente e imprimir\n");
+        op = ent.nextInt();
 
-            while ( (i > -1) && ( lista [i] > aux ) ) {
-                lista[i+1] = lista[i];
-                i--;
-            }
+        switch (op){
+            case 1:
+                lista = insertSortAsc(lista);
+                imprimir(lista);
+                break;
+            case 2:
+                lista = insertSortDesc(lista);
+                imprimir(lista);
+                break;
 
-            lista[i+1] = aux;
+            default:
+                System.out.println("Selecionar opcao de 1 ou 2");
+                break;
         }
     }
+    public static int[] insertSortAsc(int l2[]) {
+        int n ,j;
+        n=l2.length;
+        for (j=1 ; j < n ; j++){
+            int aux = l2[j];
+            int i = j-1;
+            while ( (i > -1) && ( l2 [i] > aux ) ) {
+                l2[i+1] = l2[i];
+                i--;
+            }
+            l2[i+1] = aux;
+        }
+        return l2;
+    }
 
-    public void insertSortDesc(){
-
+    public static int[]  insertSortDesc(int l3[]){
+        int m,k;
+        m = l3.length;
+        for (k=1 ; k < m ; k++){
+            int aux = l3[k];
+            int i = k-1;
+            while ( (i > -1) && ( l3 [i] < aux ) ) {
+                l3[i+1] = l3[i];
+                i--;
+            }
+            l3[i+1] = aux;
+        }
+        return l3;
+    }
+    public static void imprimir(int l[]){
+        int i;
+        for (i=0; i<l.length; i++){
+            System.out.print(l[i]+" ");
+        }
     }
 }
